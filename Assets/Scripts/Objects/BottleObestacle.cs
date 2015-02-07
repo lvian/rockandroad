b4aa9ab9c7c1e187166e2e11bea442406bae7ed5
lvian <leandrovian@hotmail.com>
2015-02-07 02:28:54 -0200

    Added bottle obstacle

---
+++ Assets/Scripts/Objects/BottleObestacle.cs
@@ -0,0 +1,46 @@
+using UnityEngine;
+using System.Collections;
+
+public class BottleObestacle : MovableObstacle {
+
+	// Use this for initialization
+	public int points = 25;
+	public float sp = 1.5f;
+
+
+
+	private Animator anim;
+	// Update is called once per frame
+
+	public override void Start()
+	{
+		base.Start ();
+		anim = GetComponentInChildren<Animator>();
+	}
+
+	void Update () {
+		if(gc.currentGameState == GameControl.GameState.Play)
+		{
+			anim.speed = 1;
+			movement(points, sp);
+		}else if (gc.currentGameState == GameControl.GameState.Pause)
+		{
+			anim.speed = 0;
+		}
+	}
+
+	#region implemented abstract members of SpawnableObject
+
+	public override void onCollide (Player p)
+	{
+		p.addEffect(new DungEffect(p, 2f));
+		p.Energy -= 15;
+		p.popEnergyText("-" + 15, Color.red);
+		NGUITools.PlaySound(hitSound, 0.5f);
+		p.popEffectText("SLOW", Color.magenta);
+		GetComponentInChildren<Animator> ().enabled = false;
+		GetComponentInChildren<SpriteRenderer> ().enabled = false;
+	}
+
+	#endregion
+}
