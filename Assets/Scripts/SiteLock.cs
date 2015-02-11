0846ce1036cbf93f0a5a48dd81fe381f68682c9b
lvian <leandrovian@hotmail.com>
2015-02-11 16:50:12 -0200

    Added site lock to the game

---
+++ Assets/Scripts/SiteLock.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SiteLock : MonoBehaviour {
+	
+	public string[] allowedSites;
+	
+	void Start () {
+		
+		if (Application.isWebPlayer)
+		{
+			string url = Application.absoluteURL.Split('/')[2];
+			bool foundGoodSite = false;
+			Debug.Log(url);
+			for (int i = 0; i < allowedSites.Length; i++)
+			{
+				if (url.IndexOf(allowedSites[i]) >= 0 && url.IndexOf(allowedSites[i]) == (url.Length - allowedSites[i].Length))
+				{
+					foundGoodSite = true;
+				}
+			}
+			
+			if (foundGoodSite == false)
+			{
+				//site is invalid, lock us up!
+				Application.Quit();
+			}
+		}
+	}
+}
