65cccebec51f33e090562cc2087dc64d2df1a690
lvian <leandrovian@hotmail.com>
2015-02-11 11:15:51 -0200

    New static obstacles, changes in the gameplay GUI

---
+++ Assets/Scripts/Objects/BoxObstacle.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+using System.Collections;
+
+public class BoxObstacle : Obstacle {
+
+	// Use this for initialization
+	public int points = 15;
+	public float sp = 1.0f;
+
+	// Update is called once per frame
+	void Update () {
+		if(gc.currentGameState == GameControl.GameState.Play)
+		{
+			movement(points , sp);
+		}
+	}
+
+	#region implemented abstract members of SpawnableObject
+
+	public override void onCollide (Player p)
+	{
+		p.Energy -= points;
+		p.popEnergyText("-" + points, Color.red);
+		NGUITools.PlaySound(hitSound, 0.5f);
+	}
+
+	#endregion
+}
